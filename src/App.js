a58465a3150bc46cfc3d0319db07edf2c1735298
Jeffrey Platt <jsplatt@gmail.com>
2021-08-19 15:08:07 -0400

    header started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './Header';
 
 function App() {
   return (
@@ -14,7 +10,7 @@ function App() {
       <>
         <Switch>
           <Route path="/" exact>
-            <h1>This is the homepage</h1>
+            <Header />
           </Route>
         </Switch>
       </>
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import  styled  from 'styled-components'
+import React from 'react'
+import { Avatar } from '@material-ui/core'
+
+function Header() {
+    return (
+        <HeaderContainer>
+            {/* Header left */}
+            <HeaderLeft>
+
+            </HeaderLeft>
+            {/* Header search */}
+
+            {/* Header right */}
+            
+        </HeaderContainer>
+    )
+}
+
+export default Header
+
+const HeaderContainer = styled.div``;
+
+const HeaderLeft = styled.div``;
