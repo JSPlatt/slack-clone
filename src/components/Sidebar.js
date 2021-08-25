1eb07036f697fb983694a0e0958709b1308069d9
Jeffrey Platt <jsplatt@gmail.com>
2021-08-25 10:15:26 -0400

    sidebar started

---
+++ src/components/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from "styled-components"
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+
+function Sidebar() {
+    return (
+        <SidebarContainer>
+            <SidebarHeader>
+                <SidebarInfo>
+                    <h2>SLACK CLONE HQ</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Jeff Platt
+                    </h3>
+                </SidebarInfo>
+            </SidebarHeader>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
+
+const SidebarContainer = styled.div `
+    background-color: var(--slack-color);
+`
+const SidebarHeader = styled.div`
+
+`
+const SidebarInfo = styled.div`
+
+`
