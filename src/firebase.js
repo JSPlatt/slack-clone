2b648a108d9aa9cae28c3d39c2c2aab09ca693c4
Jeffrey Platt <jsplatt@gmail.com>
2021-08-25 13:49:16 -0400

    firebase db, auth added

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase"
+
 const firebaseConfig = {
     apiKey: "AIzaSyAQg92XaXKp_4xtCXqUMpEu6L_98hXCOr0",
     authDomain: "slack-clone-fe3db.firebaseapp.com",
@@ -8,3 +9,8 @@ const firebaseConfig = {
     appId: "1:842498015760:web:c8a422ffa2ad9ec4328095",
     measurementId: "G-J3C7QV0MEX"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
