c2506014eab5747d955c1979203fc207a53ba176
Jeffrey Platt <jsplatt@gmail.com>
2021-08-25 13:33:38 -0400

    adding channels

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,6 +12,7 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
 import AppsIcon from '@material-ui/icons/Apps'
 import ExpandLessIcon from '@material-ui/icons/ExpandLess'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
 
 function Sidebar() {
     return (
@@ -36,7 +37,10 @@ function Sidebar() {
             <SidebarOption Icon={ExpandLessIcon} title="Show less" />
             <hr />
             <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
-
+            <hr />
+            <SidebarOption Icon=
+            {AddIcon} addChannelOption title = "Add Channel"
+            />
         </SidebarContainer>
     )
 }
@@ -50,6 +54,12 @@ const SidebarContainer = styled.div `
     border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b;
+    }
 `
 const SidebarHeader = styled.div`
     display: flex;
